cd0bf26bf85fd4ae6cacd81f5eff0fde84c713fc
Irvan Mlambo <irvan.mlambo@structureit.net>
2025-07-11 15:25:16 +0200

    Update private keys

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,7 +1,12 @@
+resource "aws_key_pair" "deployer" {
+  key_name   = "githubkeys"
+  public_key = file("${path.module}/githubkeys.pub")
+}
+
 resource "aws_instance" "app_server" {
   ami           = var.ami_id
   instance_type = "t2.micro"
-  key_name      = var.key_name
+  key_name      = aws_key_pair.deployer.key_name
 
   tags = {
     Name = "devops-app-server"
