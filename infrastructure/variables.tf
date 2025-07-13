f011ab9989f9d58f87b686657bb14f82224eb84c
Irvan Mlambo <irvan.mlambo@structureit.net>
2025-07-13 11:51:17 +0200

    Changed RDS master password

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -14,5 +14,5 @@ variable "db_username" {
 
 variable "db_password" {
   description = "Password for the RDS database"
-  default     = "DevOps123!"  # Known working format for AWS RDS
+  sensitive   = true
 }
