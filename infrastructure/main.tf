6c73f141dc3ea1f95d8166da68cf2306d87f7bf6
Irvan Mlambo <irvan.mlambo@structureit.net>
2025-07-13 11:27:09 +0200

    Use datasource instead of hardcoded ami id

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,13 +3,29 @@ resource "aws_key_pair" "deployer" {
   public_key = file("${path.module}/githubkeys.pub")
 }
 
+# Get the latest Ubuntu 22.04 LTS AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 # Use data source to reference existing security group
 data "aws_security_group" "app_server_sg" {
   name = "app-server-sg"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = var.ami_id
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   key_name      = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [data.aws_security_group.app_server_sg.id]
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -2,11 +2,6 @@ variable "region" {
   default     = "eu-west-1"
 }
 
-variable "ami_id" {
-  description = "AMI ID for the EC2 instance"
-  default     = "ami-0022cd1b90baccceb"  # Ubuntu 22.04 LTS in eu-west-1
-}
-
 variable "key_name" {
   description = "Name of the SSH key pair to use for the EC2 instance"
   default     = "my-key-pair" # Replace with your actual key pair name
